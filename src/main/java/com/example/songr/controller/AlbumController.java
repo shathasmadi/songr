abad9899dc807d1a9baa45e89902f594198ae79c
shathasmadi <alsmadishatha@gmail.com>
2021-08-09 19:22:46 +0300

    lab12

--- src/main/java/com/example/songr/controller/AlbumController.java
+++ src/main/java/com/example/songr/controller/AlbumController.java
@@ -3,9 +3,12 @@ import com.example.songr.model.Album;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.view.RedirectView;
+
 
 
 
@@ -17,15 +20,6 @@ public class AlbumController {
     @GetMapping("/Album")
    public String albums (Model modelOne) {
 
-//        Album one = new Album("ya ana ya la","Amr Diab",5,10,"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQy3wqjzyfgaZFLvrwDY7Px4pgLrcZdcHQ0Ig&usqp=CAU");
-//        Album two = new Album("3ala bali","shereen abdelwahab",4,20,"https://upload.wikimedia.org/wikipedia/commons/4/49/%D8%B4%D9%8A%D8%B1%D9%8A%D9%86.jpg");
-//        Album three = new Album("salamat","Nancy",9,80,"https://yt3.ggpht.com/1qwv3D_Q-taogErjMjeicbrKjL7kGJhBXDnsbY7ZMGsaJS88xW2aIqtyqDpUNxZWvYnfdp8R=s900-c-k-c0x00ffffff-no-rj");
-//
-//        List<Album> albumsTest = new ArrayList<>();
-//
-//              albumsTest.add(one);
-//              albumsTest.add(two);
-//              albumsTest.add(three);
 
               modelOne.addAttribute("albums",albumRepository.findAll());
 
@@ -34,8 +28,8 @@ public class AlbumController {
     }
 
     @PostMapping("/Album")
-       public RedirectView addAlbum(String title, String artist, int songCount, int length, String imageUrl) {
-       Album album = new Album(title, artist, songCount, length, imageUrl);
+       public RedirectView addAlbum(@RequestBody MultiValueMap<String, String> data) {
+       Album album = new Album(data.get("title").get(0),data.get("artist").get(0),data.get("songCount").get(0),data.get("length").get(0),data.get("imageUrl").get(0));
        albumRepository.save(album);
        return new RedirectView("/Album");
 }
--- src/main/java/com/example/songr/model/Album.java
+++ src/main/java/com/example/songr/model/Album.java
@@ -8,14 +8,14 @@ import javax.persistence.Id;
 public class Album {
    private  String title;
    private String artist;
-   private  int songCount;
-   private  int length ;
+   private  String songCount;
+   private String length ;
     private  String imageUrl;
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private int id;
 
-    public Album(String title, String artist, int songCount, int length, String imageUrl) {
+    public Album(String title, String artist, String songCount, String length, String imageUrl) {
         this.title = title;
         this.artist = artist;
         this.songCount = songCount;
@@ -43,19 +43,19 @@ public class Album {
         this.artist = artist;
     }
 
-    public int getSongCount() {
+    public String getSongCount() {
         return songCount;
     }
 
-    public void setSongCount(int songCount) {
+    public void setSongCount(String songCount) {
         this.songCount = songCount;
     }
 
-    public int getLength() {
+    public String getLength() {
         return length;
     }
 
-    public void setLength(int length) {
+    public void setLength(String length) {
         this.length = length;
     }
 
